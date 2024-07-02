77f5604bc7fca4e806ff730ccf525a98d7cc0bf4
Noah_Dave <m.davenoa@gmail.com>
2024-07-02 23:11:12 +0300

    Initial commit

---
+++ goals.js
@@ -0,0 +1,26 @@
+//Menu Icon 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navMenu = document.querySelector('nav ul');
+
+    menuIcon.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
+
+// Function to update current time and day
+function updateDateTime() {
+    const currentDate = new Date();
+    // Local Time
+    const day = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
+    const localTime = currentDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+    // UTC Time
+    const utcTime = currentDate.toISOString().slice(11, 19); // Extracting HH:mm:ss from ISO string
+
+    // Update DOM elements
+    document.getElementById('current-time').textContent = `${day}, ${localTime}`;
+    document.getElementById('current-time-utc').textContent = `UTC Time: ${utcTime}`;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
